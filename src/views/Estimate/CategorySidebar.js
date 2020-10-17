3d82e829c7b69bbc7e79526385383fae9da9f335
Constantin Zoo <70200415+shadow77848248@users.noreply.github.com>
2020-10-17 20:22:16 +0300

    side modal group

--- src/views/Estimate/CategorySidebar.js
+++ src/views/Estimate/CategorySidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GroupModal from './Categories/GroupModal';
 import PaverModal from './Categories/PaverModal';
@@ -9,6 +9,8 @@ import CinderBlockModal from './Categories/CinderBlockModal';
 import LightsModal from './Categories/LightsModal';
 import MaterialsModal from './Categories/MaterialsModal';
 import FireplaceModal from './Categories/FireplaceModal';
+import Button from "components/CustomButtons/Button.js";
+import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
 
 const useStyles = makeStyles({
     root: {
@@ -28,40 +30,48 @@ const useStyles = makeStyles({
 });
 
 export default function SimpleCard() {
+
+    const [isSide, setSide] = useState(false);
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
 
     return (
         <div className="side-modal-bar">
-            <ul>
-                <li>
-                    <GroupModal />
-                </li>
-                <li>
-                    <PaverModal />
-                </li>
-                <li>
-                    <SegmentalModal />
-                </li>
-                <li>
-                    <NaturalStoneModal />
-                </li>
-                <li>
-                    <EdgeStoneModal />
-                </li>
-                <li>
-                    <CinderBlockModal />
-                </li>
-                <li>
-                    <LightsModal />
-                </li>
-                <li>
-                    <MaterialsModal />
-                </li>
-                <li>
-                    <FireplaceModal />
-                </li>
-            </ul>
-        </div>
-    );
+            <div className={ isSide ? "side-bar-body" : "side-bar-body hidden-side"}>
+                <div className="toggle-icon">
+                            <Button variant="contained" color="success" component="span" size="sm" onClick={e => setSide(!isSide)}>
+                                <DragIndicatorIcon />
+                            </Button>
+                        </div>
+                        <ul>
+                            <li>
+                                <GroupModal />
+                            </li>
+                            <li>
+                                <PaverModal />
+                            </li>
+                            <li>
+                                <SegmentalModal />
+                            </li>
+                            <li>
+                                <NaturalStoneModal />
+                            </li>
+                            <li>
+                                <EdgeStoneModal />
+                            </li>
+                            <li>
+                                <CinderBlockModal />
+                            </li>
+                            <li>
+                                <LightsModal />
+                            </li>
+                            <li>
+                                <MaterialsModal />
+                            </li>
+                            <li>
+                                <FireplaceModal />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            );
 }
